e7cf92cb526cb9c13c01eb2398111eb5462ae007
Vinothkumar.M <102602070+vinosmart@users.noreply.github.com>
2022-11-14 15:10:31 +0530

    updated-layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
 	import '../app.postcss';
+	import Sidebar from './Sidebar.svelte';
 
 </script>
 
 <div class="app">
 	
-
-	<main>
+	<main class="flex bg-[#2F4858]/95">
+		<Sidebar />
 		<slot />
 	</main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,103 +1,45 @@
 <script>
-	let profilename='VINOTHKUMAR'
-	let Positionheld='Front End Developer'
+	import Sidebar from './Sidebar.svelte';
 </script>
 
 <main class="">
-	<img src="/bag.jpg" class="absolute -mt-[300px]"  alt="">
-	<div
-		class="lg:w-[400px] h-[720px] flex flex-col items-center lg:ml-[312px] lg:mt-[300px] pb-20  w-full  lg:mb-0 mx-auto relative bg-white text-center lg:border px- rounded-[20px] mt-56  lg:mt-[300px] "
-	>
-		<div>
-			<img
-				src="./profile.jpg"
-				class=" w-[180px] h-[180px] lg:w-[240px] z-20  absolute transform -translate-x-[50%] lg:h-[240px]  drop-shadow-xl mx-auto rounded-full lg:-mt-[140px] -mt-56"
-				alt=""
-			/>
-		</div>
-		<div>
-			<h1 class=" lg:mt-32  text-2xl font-semibold">{profilename}</h1>
-			<p class="text-[#7b7b7b] text-base mt-3">{Positionheld}</p>
-		</div>
+	<div class="flex">
 
-		<div class="flex gap-5 justify-center mt-[28px]">
-			<div>
-				<img
-					src="./facebook.svg"
-					class="lg:w-7  lg:h-7 hover:bg-[#043E96]  bg-[#7b7b7b]/10  rounded p-1.5"
-					alt=""
-				/>
-			</div>
-			<img
-				src="./instagram.svg"
-				class="lg:w-7  lg:h-7 hover:bg-[#043E96]  bg-[#7b7b7b]/10  rounded p-1.5"
-				alt=""
-			/>
-			<img
-				src="./linkedin.svg"
-				class="lg:w-7  lg:h-7 hover:bg-[#043E96]  bg-[#7b7b7b]/10  rounded p-1.5"
-				alt=""
-			/>
-			<img
-				src="./github.svg"
-				class="lg:w-7  lg:h-7 hover:bg-[#043E96]  bg-[#7b7b7b]/10  rounded p-1.5"
-				alt=""
-			/>
-		</div>
-		<div class="px-4 w-full"><div class="lg:w-[352px] lg:h-[297px] w-full bg-[#7b7b7b]/10 rounded-xl mt-10 flex flex-col px-5 py-5 ">
-			<div class="flex mt-">
-				<img
-					src="./call.svg"
-					class="lg:w-10 ml-2  lg:h-10 hover:bg-[#043E96]  bg-white  rounded p-1.5"
-					alt=""
-				/>
-				<div class="flex flex-col ml-2">
-					<p class="flex justify-start text-gray-500">Phone</p>
-					<h1 class="text-black/80 text-sm lg:text-base">7845860720</h1>
-				</div>
-			</div>
+	<div class="flex flex-col">
+		
 
-			<div class="flex mt-4">
-				<img
-					src="./email.svg"
-					class="lg:w-10 ml-2  lg:h-10 hover:bg-[#043E96]  bg-white  rounded p-1.5"
-					alt=""
-				/>
-				<div class="flex flex-col ml-2">
-					<p class="flex justify-start text-gray-500">Email</p>
-					<h1 class="text-black/80  text-sm lg:text-base">Pavinoth00@gmail.com</h1>
-				</div>
-			</div>
-			<div class="flex mt-4">
-				<img
-					src="./location.svg"
-					class="lg:w-10 ml-2  lg:h-10 hover:bg-[#043E96]  bg-white  rounded p-1.5"
-					alt=""
-				/>
-				<div class="flex flex-col ml-2">
-					<p class="flex justify-start text-gray-500">Location</p>
-					<h1 class="text-black/80  text-sm lg:text-base">Chennai</h1>
-				</div>
-			</div>
-			<div class="flex mt-4">
-				<img
-					src="./calender.svg"
-					class="lg:w-10 ml-2  lg:h-10 hover:bg-[#043E96]  bg-white  rounded p-1.5"
-					alt=""
-				/>
-				<div class="flex flex-col ml-2">
-					<p class="flex justify-start text-gray-500">Birthday</p>
-					<h1 class="text-black/80  text-sm lg:text-base">05 Mar 1996</h1>
-				</div>
-			</div>
-		</div></div>
-		<div class="flex justify-center">
-			<a href="/profile.jpg"><button
-				class="rounded-full px-8 py-3 text-lg text-white border-2 items-center flex border-[#974DE1]/90 mt-10 duration-300 transition ease-linear bg-gradient-to-r hover:bg-gradient-to-l from-[#DD2476]/90  to-[#fa5252ef]/75"
-				> <img src="/fa_download.svg" class="w-4 h-4  mr-4 " alt=""> Download CV</button
-			></a>
+		<div class="bg-white shadow-lg rounded-xl w-[840px] h-[100px]  mt-[200px] ml-10 border  ">
+			<ul class="flex   justify-center gap-10 px-4 items-center py-3 ">
+				<li
+					class=" hover:font-bold w-[130px] h-20 flex justify-center items-center rounded-2xl text-base bg-[#2F4858] text-white cursor-pointer transition-all duration-300 ease-in-out"
+				>
+					About
+				</li>
+				<li
+					class=" hover:font-bold w-[130px] h-20 flex justify-center items-center rounded-2xl text-base hover:bg-[#2F4858] hover:text-white cursor-pointer transition-all duration-300 ease-in-out"
+				>
+					Resume
+				</li>
+				<li
+					class=" hover:font-bold w-[130px] h-20 flex justify-center items-center rounded-2xl text-base hover:bg-[#2F4858] hover:text-white cursor-pointer transition-all duration-300 ease-in-out"
+				>
+					Works
+				</li>
+				<li
+					class=" hover:font-bold w-[130px] h-20 flex justify-center items-center rounded-2xl text-base hover:bg-[#2F4858] hover:text-white cursor-pointer transition-all duration-300 ease-in-out"
+				>
+					Blogs
+				</li>
+				<li
+					class=" hover:font-bold w-[130px] h-20 flex justify-center items-center rounded-2xl text-base hover:bg-[#2F4858] hover:text-white cursor-pointer transition-all duration-300 ease-in-out"
+				>
+					Contact
+				</li>
+			</ul>
 		</div>
+		<div class="bg-white shadow-lg rounded-xl w-[840px] h-[900px] mt-10 scroll-mt-12 ml-10 border flex-wrap  "></div>
 	</div>
+</div>
 </main>
 
 <style>
